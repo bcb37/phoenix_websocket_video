262ee87fb6ef4f3ced3933923d61a4859088d5be
Duc Duong <duc178@gmail.com>
2016-03-17 16:20:52 -0400

    update prod host

--- config/prod.exs
+++ config/prod.exs
@@ -8,7 +8,7 @@ use Mix.Config
 # meaningful, we use this information when generating URLs.
 config :chat, Chat.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com"]
+  url: [host: "phoenix_websocket_video.heroku.com"]
 
 # ## SSL Support
 #
